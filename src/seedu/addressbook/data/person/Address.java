4f7e97c3b39a1a36c2912211c62837f3849795ab
ruolan <fionaxrl@gmail.com>
2017-01-31 22:22:05 +0800

    Split Address into block, street, unit, postal code; Update the user guide and tests

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,9 +8,20 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+    public static final String EXAMPLE = "123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = 
+    		"Person addresses should be entered in the following format: a/BLOCK, STREET, UNIT, POSTAL_CODE ";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    
+    private static final int BLOCK_INDEX = 0;
+    private static final int STREET_INDEX = 1;
+    private static final int UNIT_INDEX = 2;
+    private static final int POSTAL_INDEX = 3;
+    
+    public Block block;
+    public Street street;
+    public Unit unit;
+    public PostalCode postalCode;
 
     public final String value;
     private boolean isPrivate;
@@ -22,18 +33,106 @@ public class Address {
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         String trimmedAddress = address.trim();
-        this.isPrivate = isPrivate;
-        if (!isValidAddress(trimmedAddress)) {
+        this.isPrivate = isPrivate;    
+        this.value = trimmedAddress;
+        
+        //split the address into block, street, unit and postal code 
+        String[] addressArr = trimmedAddress.split(","); 
+        if (!isValidAddress(addressArr)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+        
+        this.block = new Block(addressArr[BLOCK_INDEX]);
+        this.street = new Street(addressArr[STREET_INDEX]);
+        this.unit = new Unit(addressArr[UNIT_INDEX]);
+        this.postalCode = new PostalCode(addressArr[POSTAL_INDEX]);
+        
     }
-
+    
+    /**
+     * Represents block number in the person's address.
+     */
+    public class Block {
+    	private String blockNumber;
+    	
+    	public Block(String block) {
+    		this.blockNumber = block;
+    	}
+    	
+    	/**
+    	 *@return block number
+    	 */
+    	public String returnBlockNumber() {
+    		return blockNumber;
+    	}
+    	
+    }
+    
+    /**
+     * Represents street in the person's address.
+     */
+    public class Street {
+    	private String streetName;
+    	
+    	public Street(String street) {
+    		this.streetName = street;
+    	}
+    	
+    	/**
+    	 *@return street name
+    	 */
+    	public String returnStreetName() {
+    		return streetName;
+    	}
+    }
+    
+    /**
+     * Represents unit in the person's address.
+     */
+    public class Unit {
+    	private String unitNumber;
+    	
+    	public Unit(String unit) {
+    		this.unitNumber = unit;
+    	}
+    	
+    	/**
+    	 *@return unit number
+    	 */
+    	public String returnUnitNumber() {
+    		return unitNumber;
+    	}	
+    }
+    
+    /**
+     * Represents postal code in the person's address.
+     */
+    public class PostalCode {
+        private String postalCodeNumber;
+    	
+    	public PostalCode(String postalCode) {
+    		this.postalCodeNumber = postalCode;
+    	}
+    	
+    	/**
+    	 *@return postal code number
+    	 */
+    	public String returnPostalCodeNumber() {
+    		return postalCodeNumber;
+    	}
+    	
+    }
+    
+    
     /**
      * Returns true if a given string is a valid person email.
      */
-    public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX);
+    public static boolean isValidAddress(String[] test) {
+    	boolean hasAllElements, isMatchRegex;
+    	int elementNumber = test.length;
+    	hasAllElements = (elementNumber == 4 );
+    	isMatchRegex = test.toString().matches(ADDRESS_VALIDATION_REGEX);
+        return  (isMatchRegex && hasAllElements);
     }
 
     @Override
@@ -44,7 +143,7 @@ public class Address {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Address // instanceof handles nulls
+                || (other instanceof Address // instance of handles nulls
                 && this.value.equals(((Address) other).value)); // state check
     }
 
