cbe67c147d4b3e11290a1de75b6d6dc975bb8d7e
ruolan <fionaxrl@gmail.com>
2017-02-08 12:34:25 +0800

    Add a Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An immutable interface for Name, Email, Phone, Address class in the addressbook.
+ */
+public interface Printable {
+	
+	public String getPrintableString();
+
+}
